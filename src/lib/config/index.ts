978d572b51be87f1296e058873de77a918235104
pulsehexican <pulsechain555@gmail.com>
2023-07-02 04:12:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/lib/config/index.ts
@@ -0,0 +1,37 @@
+import { Config } from './types';
+
+import arbitrum from './arbitrum';
+import goerli from './goerli';
+import gnosisChain from './gnosis-chain';
+import mainnet from './mainnet';
+import optimism from './optimism';
+import polygon from './polygon';
+import zkevm from './zkevm';
+
+// We don't import Network from sdk to avoid extra bundle size when loading app (while the SDK is not tree-shakable)
+export enum Network {
+  MAINNET = 1,
+  ROPSTEN = 3,
+  RINKEBY = 4,
+  GOERLI = 5,
+  GÖRLI = 5,
+  OPTIMISM = 10,
+  KOVAN = 42,
+  GNOSIS = 100,
+  POLYGON = 137,
+  FANTOM = 250,
+  ZKEVM = 1101,
+  ARBITRUM = 42161,
+}
+
+const config: Record<Network | number, Config> = {
+  [Network.MAINNET]: mainnet,
+  [Network.GOERLI]: goerli,
+  [Network.POLYGON]: polygon,
+  [Network.ARBITRUM]: arbitrum,
+  [Network.OPTIMISM]: optimism,
+  [Network.GNOSIS]: gnosisChain,
+  [Network.ZKEVM]: zkevm,
+};
+
+export default config;
---
+++ src/services/web3/connectors/trustwallet/walletconnect.connector.ts
@@ -0,0 +1,50 @@
+import { EthereumProvider } from '@walletconnect/ethereum-provider';
+
+import { configService } from '@/services/config/config.service';
+import { WalletError } from '@/types';
+import { Connector, ConnectorId } from '../connector';
+import { Network } from '@/lib/config';
+import useDarkMode from '@/composables/useDarkMode';
+
+const { MAINNET, POLYGON, ARBITRUM, GNOSIS, ZKEVM } = Network;
+
+export class WalletConnectConnector extends Connector {
+  id = ConnectorId.WalletConnect;
+  async connect() {
+    const provider = await EthereumProvider.init({
+      projectId: 'ee9c0c7e1b8b86ebdfb8fd93bb116ca8',
+      chains: [MAINNET],
+      optionalChains: [POLYGON, ARBITRUM, GNOSIS],
+      rpcMap: {
+        [MAINNET]: configService.getNetworkRpc(MAINNET),
+        [POLYGON]: configService.getNetworkRpc(POLYGON),
+        [ARBITRUM]: configService.getNetworkRpc(ARBITRUM),
+        [GNOSIS]: configService.getNetworkRpc(GNOSIS),
+        [ZKEVM]: configService.getNetworkRpc(ZKEVM),
+      },
+      showQrModal: true,
+      qrModalOptions: { themeMode: useDarkMode().darkMode ? 'dark' : 'light' },
+    });
+    this.provider = provider;
+
+    try {
+      const accounts = await provider.enable();
+      const chainId = await provider.request({ method: 'eth_chainId' });
+      this.handleChainChanged(chainId);
+      this.handleAccountsChanged(accounts);
+    } catch (err) {
+      if ((err as WalletError).code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log('Please connect to WalletConnect.');
+      } else {
+        console.error(err);
+      }
+    }
+    return {
+      provider,
+      account: this.account,
+      chainId: this.chainId,
+    };
+  }
+}
